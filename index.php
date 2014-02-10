e1849f25701803e35642e3b823066238a0df6712
Richard Escobedo <escobedorichard@gmail.com>
2014-02-10 14:58:52 -0600

    Update meta info

--- index.php
+++ index.php
@@ -28,7 +28,7 @@
 							<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
 						</div>
 						
-						<p class="meta"><?php _e("Posted", "wpbootstrap"); ?> <time datetime="<?php echo the_time('Y-m-j'); ?>" pubdate><?php the_time(); ?></time> <?php _e("by", "wpbootstrap"); ?> <?php the_author_posts_link(); ?> <span class="amp">&</span> <?php _e("filed under", "wpbootstrap"); ?> <?php the_category(', '); ?>.</p>
+						<p class="meta"><?php _e("Posted", "wpbootstrap"); ?> <time datetime="<?php echo the_time('Y-m-j'); ?>" pubdate><?php the_time(); ?></time> <?php _e("by", "wpbootstrap"); ?> <?php the_author_posts_link(); ?> <span class="amp">&</span> <?php _e("in", "wpbootstrap"); ?> <?php the_category(', '); ?>.</p>
 				</header> <!-- end article header -->
 					
 				<section class="post_content clearfix">
