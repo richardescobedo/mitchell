37475c32228baa9b4809cc1dde0e6a3d73d88e9d
Richard Escobedo <escobedorichard@gmail.com>
2014-02-23 16:36:42 -0600

    Add static mobile welcome

--- page-home.php
+++ page-home.php
@@ -21,5 +21,22 @@ Template Name: Homepage
 			</div>
 				<?php edit_post_link( __( 'Edit' ), '<span class="edit-link">', '</span>' ); ?>
 			</div>
+				
+				<?php endwhile; ?>
+				
+			<!-- Mobile Welcome -->
+			<div class="col-xs-12 col-sm-12 col-md-8 visible-xs">
+				<div class="jumbotron">
+					<div class="container">
+						<img src="<?php echo get_bloginfo('template_directory');?>/library/i/shield.png" class="center-block" />
+						<br />
+							<div class="btn-group-vertical center-block">
+							<a href="http://tcuphikaps.com/about" type="button" class="center-block btn btn-primary btn-lg">Learn about us <i class="fa fa-angle-right"></i> </a>	
+							<a href="http://tcuphikaps.com/recruitment type="button" class="center-block btn btn-primary btn-lg">Become a Phi Kap <i class="fa fa-angle-right"></i> </a>
+							</div>		
+					</div>		
+				</div>
+			</div>
+				
 <?php get_sidebar();?>
 <?php get_footer();?>
