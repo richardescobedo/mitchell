a27649fc2b7c19a7816bdd345bbb97703a0070ba
Richard Escobedo <escobedorichard@gmail.com>
2014-02-27 00:10:43 -0600

    Markup cleanup

--- index.php
+++ index.php
@@ -1,20 +1,6 @@
-<?php 
-/**
- * The main template file.
- *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- * Learn more: http://codex.wordpress.org/Template_Hierarchy
- *
- * @package Shape
- * @since Shape 1.0
- */
- 
- get_header(); ?>
+<?php get_header(); ?>
 	
-	<!-- Begin Post Content -->
+	<!-- Begin page content -->
 	<div class="container">
 		<div class="row">
 			<div class="col-lg-8 text-justify">
