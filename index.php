30ff011b9688afa72f05a1770531bcfa55b16e8e
Richard Escobedo <escobedorichard@gmail.com>
2014-02-27 00:15:29 -0600

    Build page navigation using Bootstrap components

--- index.php
+++ index.php
@@ -31,6 +31,18 @@
 			
 				<?php endwhile; ?>
 				
+				<?php if (function_exists('page_navi')) { // if expirimental feature is active ?>
+				
+					<?php page_navi(); // use the page navi function ?>
+					
+				<?php } else { // if it is disabled, display regular wp prev & next links ?>
+						<nav class="wp-prev-next">
+							<ul class="pager">
+								<li class="previous"><?php next_posts_link(_e('<i class="fa fa-long-arrow-left"></i> Older Entries', "wpbootstrap")) ?></li>
+								<li class="next"><?php previous_posts_link(_e('Newer Entries <i class="fa fa-long-arrow-right"></i>', "wpbootstrap")) ?></li>
+							</ul>
+						</nav>
+					<?php } ?>		
 					
 				<?php edit_post_link( __( 'Edit' ), '<span class="edit-link">', '</span>' ); ?>
 					
