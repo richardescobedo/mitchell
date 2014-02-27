0aaae91f72813ecdc2e17cebc617ad108d8c26b4
Richard Escobedo <escobedorichard@gmail.com>
2014-02-27 00:19:11 -0600

    Rebuild page Loop to conform to HTML 5 standards
    Added button to edit pages, and schemea SEO tags

--- page.php
+++ page.php
@@ -1,32 +1,30 @@
-<?php
-/**
- * The main template file.
- *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- * Learn more: http://codex.wordpress.org/Template_Hierarchy
- *
- * @package Shape
- * @since Shape 1.0
- */
- 
-get_header(); ?>
+<?php get_header(); ?>
 
-	<div class="container"><?php /* Start the Loop */ ?>
-<?php while ( have_posts() ) : the_post(); ?>
+	<!-- Begin page content -->
+	<div class="container">
+	
+	<?php /* Start the Loop */ if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
      
 		<div class="row">
 			<div class="col-lg-8 text-justify">
-			<h1><?php the_title(); ?></h1>
-			<p><?php the_content(); ?></p>
-			
-			<?php edit_post_link( __( 'Edit' ), '<span class="edit-link">', '</span>' ); ?>
-		</div>	
-
-
-	<?php endwhile; ?>
+				<article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
+					<header>
+						<div class="page-header"><h1 class="page-title" itemprop="headline"><?php the_title(); ?></h1></div>
+					</header>
+					
+					<section class="post_content clearfix" itemprop="articleBody">
+					<?php the_content(); ?>
+					</section> 
+					
+					<!-- End article -->
+	
+					<a href="<?php echo get_edit_post_link( $id, $context ); ?>"><button class="btn btn-primary btn-lg pull-right"><strong>Edit this page</strong></button></a>
+					
+				</article>
+				
+				<?php endwhile; else: ?>
+					
+					<article id="post-not-found">
 	
 <?php get_sidebar();?>
 <?php get_footer();?>
