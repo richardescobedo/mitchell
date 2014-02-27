9ddd23d8055b8fd9dbdcbae7fde642db5eab8c60
Richard Escobedo <escobedorichard@gmail.com>
2014-02-27 00:21:40 -0600

    Globalized search form.php to work across all pages/posts

--- searchform.php
+++ searchform.php
@@ -1,9 +1,9 @@
-<form action="<?php echo home_url( '/' ); ?>" method="get" class="form-inline">
+<form class="hidden-sm hidden-xs" role="form" action="<?php echo home_url( '/' ); ?>" method="get" class="form-inline">
     <fieldset>
 		<div class="input-group">
 			<input type="text" name="s" id="search" placeholder="<?php _e("Looking for something?","wpbootstrap"); ?>" value="<?php the_search_query(); ?>" class="form-control" />
 			<span class="input-group-btn">
-				<button type="submit" class="btn btn-default"><i class="fa fa-search fa-fw"></i></button>
+				<button type="submit" class="btn btn-default"><?php _e('<i class="fa fa-search fa-fw"></i>',"wpbootstrap"); ?></button>
 			</span>
 		</div>
     </fieldset>
