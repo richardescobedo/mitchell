32a1d9847752af72b4e455558c2535ffdeaff2e2
Richard Escobedo <escobedorichard@gmail.com>
2014-02-28 12:10:13 -0600

    Alter footer styling

--- footer.php
+++ footer.php
@@ -2,10 +2,12 @@
 		<div class="footer-top clearfix">
 			<div class="footer-bottom">
 				<div class="container">
-					<div class="navbar-text pull-left">
-					All Content &copy; Copyright 2014, Phi Kappa Sigma - Beta Theta. <br />Designed and produced by Richard Escobedo | Built with Bootstrap and Wordpress
+					<div class="navbar-text navbar-left">
+					<p>All Content &copy; Copyright 2014, Phi Kappa Sigma - Beta Theta.</p>
+					<p>Designed and produced by <a href="http://richard-escobedo.com">Richard Escobedo</a>. Built with <a href="http://getbooststrap.com">Bootstrap</a>.</p>
 					</div>
-					<div class="navbar-text pull-right">
+					<div class="navbar-text navbar-right">
+						<p>
 						<a href="https://www.facebook.com/groups/64416576792/"><button type="button" class="btn btn-primary"><i class="fa fa-facebook"></i></button></a>
 						<a href="http://www.linkedin.com/groupInvitation?groupID=156115&sharedKey=1584AC73A27B"><button type="button" class="btn btn-primary"><i class="fa fa-linkedin"></i></button></a>
 						<a href="http://pks.org"><button type="button" class="btn btn-primary">ΦΚΣ</button>
