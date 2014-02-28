174859a5fbf623be4e2f4bae3618fdd614fee502
Richard Escobedo <escobedorichard@gmail.com>
2014-02-28 12:27:56 -0600

    Place Beta Theta Bulletin in a panel

--- sidebar.php
+++ sidebar.php
@@ -2,25 +2,26 @@
 			<div class="col-xs-12 col-sm-12 col-md-4 text-justify">
 			
 				<?php get_search_form(); ?>
+				<br />
+				<div class="panel panel-default clearfix">
+				 <?php /*Panels loop begins*/?> 
+					<div class="panel-heading"><h3 class="panel-title"><span>Beta Theta Bulletin</span></h3></div>
+						<div class="panel-body">
+							<?php $the_query = new WP_Query( 'showposts=3' );?>
+							<?php while ($the_query -> have_posts()) : $the_query -> the_post(); ?>
+						
+							<article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
+								<header><a href="<?php the_permalink() ?>"><h4><?php the_title(); ?></h4></a></header>
+						
+								<section><?php the_excerpt(__('(more…)')); echo string_limit_words($excerpt,2); ?></section>
+								
+								<?php if( $the_query->current_post < $the_query->post_count-1 ) echo '<hr />'; ?>
+								
+							</article>
+							<?php endwhile;?>
+						</div>
+				</div>
 				
-				<img src="http://placehold.it/400x400&text=Maltese+Cross+Newsletter" class="display-block img-responsive img-rounded hidden-sm hidden-xs" />
-				
-				<div class="page-header"><h3><span>Beta Theta Bulletin</span></h3></div>
-
-						<?php $the_query = new WP_Query( 'showposts=3' );?>
-						<?php while ($the_query -> have_posts()) : $the_query -> the_post(); ?>
-						
-					<article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
-						<header><a href="<?php the_permalink() ?>"><h4><?php the_title(); ?></h4></a></header>
-						
-						<section><?php the_excerpt(__('(more…)')); echo string_limit_words($excerpt,2); ?></section>
-						
-						<?php if( $the_query->current_post < $the_query->post_count-1 ) echo '<hr />'; ?>
-						
-					</article>
-						<?php endwhile;?>
-
-			<br /><br />
 			</div>
 			
 		</div>
