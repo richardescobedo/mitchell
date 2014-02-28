b86ea065457597552c23cb01418f3f6a6e2f1da1
Richard Escobedo <escobedorichard@gmail.com>
2014-02-28 12:09:09 -0600

    Corrected issue #22

--- 404.php
+++ 404.php
@@ -10,10 +10,22 @@
 					
 					<p>We're going to look into what went wrong here, but in the mean time, you can <a href="http://tcuphikaps.com/contact">drop us a line</a> with any questions, or search for what you were looking for using the search box below.</p>
 					
-					<?php get_search_form(); ?>
+					<p>
+						<form class="hidden-sm hidden-xs" role="form" action="<?php echo home_url( '/' ); ?>" method="get" class="form-inline">
+							<fieldset>
+								<div class="input-group">
+									<input type="text" name="s" id="search" placeholder="<?php _e("Looking for something?","wpbootstrap"); ?>" value="<?php the_search_query(); ?>" class="form-control" />
+									<span class="input-group-btn">
+										<button type="submit" class="btn btn-default"><?php _e('<i class="fa fa-search fa-fw"></i>',"wpbootstrap"); ?></button>
+									</span>
+								</div>
+							</fieldset>
+						</form>
+					</p>
 					
+					<p><small>Chances are, though, Alex Shelly broke the site.</small></p>
 					</section>
 			</div>	
 	
-<?php get_sidebar();?>
+<?php include(TEMPLATEPATH."/sidebar-sidebar2.php");?>
 <?php get_footer();?>
