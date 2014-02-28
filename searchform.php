4dd47d795488ad1b3f791f58c02dcb3f72da25e3
Richard Escobedo <escobedorichard@gmail.com>
2014-02-28 12:26:02 -0600

    Corrected issue #22

--- searchform.php
+++ searchform.php
@@ -1,4 +1,4 @@
-<form class="hidden-sm hidden-xs" role="form" action="<?php echo home_url( '/' ); ?>" method="get" class="form-inline">
+<form role="form" action="<?php echo home_url( '/' ); ?>" method="get" class="form-inline">
     <fieldset>
 		<div class="input-group">
 			<input type="text" name="s" id="search" placeholder="<?php _e("Looking for something?","wpbootstrap"); ?>" value="<?php the_search_query(); ?>" class="form-control" />
@@ -7,7 +7,5 @@
 			</span>
 		</div>
     </fieldset>
-    
-    <hr />
 </form>
 
