94540b44bc6e9653e1534d4d06d2084363348f09
Richard Escobedo <escobedorichard@gmail.com>
2014-03-02 19:04:22 -0600

    Override index template on story page
    Fixed #28

--- functions.php
+++ functions.php
@@ -670,3 +670,36 @@ function bootstrap_breadcrumbs() {
  
 	}
 } // end bootstrap_breadcrumbs()
+
+// Allow template to be used on post page
+
+function posts_page_custom_template($template)
+{
+	global $wp_query;
+	#	find if a page_for_posts have been set.
+	if( true == ( $posts_per_page_id = get_option('page_for_posts')) ){
+		#	get the current page id.
+		$page_id = $wp_query->get_queried_object_id();
+		#	if the same page, get the correct template.
+		if( $page_id == $posts_per_page_id ){
+			#	get the current theme directory.
+			$theme_directory = get_stylesheet_directory() ."/";
+			#	get the page template
+			$page_template   = get_post_meta($page_id, '_wp_page_template', true );
+			#	by-pass the default template, allow wordpress to handle the fallback template.
+			if( $page_template != 'default' ){
+				#	find the template in the parent if the template is not a child template
+				if( is_child_theme() && !file_exists($theme_directory . $page_template) ){
+					#	set to parent template directory 
+					$theme_directory = get_template_directory();
+				}
+				return $theme_directory . $page_template;
+			}
+		}
+	}
+	return $template;
+}
+
+add_filter('template_include', 'posts_page_custom_template');
+
+?>
